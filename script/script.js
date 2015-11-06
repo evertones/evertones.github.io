c2844b0e07370384a04d02125127c7ea1a3d6001
Everton Schneider <everton.schneider@gmail.com>
2015-11-06 10:53:04 -0200

    Initial commit to build the website. Resolves #1

---
+++ script/script.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+	$("#btn_cv").click(function(){
+		show_info("cv");
+		//hide_mainbar();
+		//hide_all();
+		//$("#cv").show();
+	});
+
+	$("#btn_social_network").click(function(){
+		show_info("social_network");
+		//hide_mainbar();
+		//hide_all();
+		//$("#social_network").show();
+	});
+	
+	$("#btn_code_repo").click(function(){
+		show_info("code_repo");
+		//hide_mainbar();
+		//hide_all();
+		//$("#code_repo").show();
+	});
+	
+	$("#btn_contact").click(function(){
+		show_info("contact");
+		//hide_mainbar();
+		//hide_all();
+		//$("#contact").show();
+	});
+});
+
+function show_info(id) {
+	hide_mainbar();
+	hide_all();
+	$("#"+id).show();
+}
+
+/*
+Function to set the page to its original state.
+*/
+function show_mainpage() {
+	hide_all();
+	show_mainbar();
+}
+
+/*
+Function to show the page header when there's no info detailed.
+*/
+function show_mainbar() {
+	$("#smallbar").hide();
+	
+	$("#languages").show();
+	$("#myself").show();
+}
+
+/*
+Function to hide the page header when some info is detailed. 
+*/
+function hide_mainbar() {
+	$("#languages").hide();
+	$("#myself").hide();
+	
+	$("#smallbar").show();
+}
+
+/*
+Function to hide all divs with detailed information.
+*/
+function hide_all() {
+	$("#cv").hide();
+	$("#social_network").hide();
+	$("#code_repo").hide();
+	$("#contact").hide();
+}
+
